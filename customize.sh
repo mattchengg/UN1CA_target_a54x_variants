e48c2f416a1bc8e94f7822b88c33da679e51ab3c
mattchengg <mattcheng20080205@gmail.com>
2026-01-06 11:12:03 +0800

    target: a54x: patches: variants: disable esim for a5460/e

--- customize.sh
+++ customize.sh
@@ -4,6 +4,8 @@ SET_METADATA "vendor" "tee" 0 2000 755 "u:object_r:tee_file:s0"
 
 if ! grep -q "init_33_0 tee_file" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
     {
+        echo "(allow init_33_0 system_file (file (mounton)))"
+        echo "(allow init_33_0 system_file (dir (mounton)))"
         echo "(allow init_33_0 tee_file (dir (mounton)))"
         echo "(allow priv_app_33_0 tee_file (dir (getattr)))"
         echo "(allow init_33_0 vendor_fw_file (file (mounton)))"
